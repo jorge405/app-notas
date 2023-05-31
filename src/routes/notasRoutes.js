13e28ca2223c074e4cff54d2b94549d9261bea1b
jorge405 <jorgepomsjhope7@gmail.com>
2023-05-30 20:21:35 -0400

    subiendo cambio de ruta verOneNota

--- src/routes/notasRoutes.js
+++ src/routes/notasRoutes.js
@@ -3,7 +3,7 @@ import {VerNotas,VerOneNota,postNotas,patchNotas,deleteNotas} from '../controlle
 const routerNotas=Router()
 
 routerNotas.get('/VerNotas',VerNotas)
-routerNotas.get('/VerOneNota/:id',VerNotas)
+routerNotas.get('/VerOneNota/:id',VerOneNota)
 routerNotas.post('/postNotas',postNotas)
 routerNotas.patch('/patchNotas',patchNotas)
 routerNotas.delete('/deleteNotas',deleteNotas)
